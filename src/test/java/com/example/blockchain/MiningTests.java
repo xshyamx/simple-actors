446819055312672a29472bafd6707e6dba92c4a4
shyam <shyamsundar.shanmugam@wipro.com>
2021-11-16 18:42:24 +0530

    passing unit tests

--- src/test/java/com/example/blockchain/MiningTests.java
+++ src/test/java/com/example/blockchain/MiningTests.java
@@ -18,8 +18,20 @@ public class MiningTests {
         WorkerBehavior.Command message = new WorkerBehavior.Command(block, 0, 5);
         actor.run(message);
         var logs = actor.getAllLogEntries();
-        assertEquals(logs.size(), 1);
-        assertEquals(logs.get(0).message(), "null");
-        assertEquals(logs.get(0).level(), Level.DEBUG);
+        assertEquals(1, logs.size());
+        assertEquals("null", logs.get(0).message());
+        assertEquals(Level.DEBUG, logs.get(0).level());
+    }
+    @Test
+    void nonceInRange() {
+        var actor = BehaviorTestKit.create(WorkerBehavior.create());
+        Block block = BlocksData.getNextBlock(0, "0");
+        WorkerBehavior.Command message = new WorkerBehavior.Command(block, 4385400, 5);
+        actor.run(message);
+        var logs = actor.getAllLogEntries();
+        String expectedResult = "4385438 : 000005063c2755396873ec402b09e910c46791dd06acb720cb6ca392ed6e613f";
+        assertEquals(1, logs.size());
+        assertEquals(expectedResult, logs.get(0).message());
+        assertEquals(Level.DEBUG, logs.get(0).level());
     }
 }
