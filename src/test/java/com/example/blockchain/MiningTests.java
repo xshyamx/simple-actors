2b1251eb59c40fd68de505c5e1fc4aa0428240db
shyam <shyamsundar.shanmugam@wipro.com>
2021-11-16 18:36:12 +0530

    first working test for failure

--- src/test/java/com/example/blockchain/MiningTests.java
+++ src/test/java/com/example/blockchain/MiningTests.java
@@ -1,12 +1,25 @@
 package com.example.blockchain;
 
+import akka.actor.testkit.typed.javadsl.BehaviorTestKit;
+import com.example.blockchain.actors.WorkerBehavior;
+import com.example.blockchain.model.Block;
+import com.example.blockchain.utils.BlocksData;
 import org.junit.jupiter.api.Test;
+import org.slf4j.event.Level;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MiningTests {
     @Test
-    void example() {
-        fail();
+    void nonceNotInRange() {
+        var actor = BehaviorTestKit.create(WorkerBehavior.create());
+        Block block = BlocksData.getNextBlock(0, "0");
+        WorkerBehavior.Command message = new WorkerBehavior.Command(block, 0, 5);
+        actor.run(message);
+        var logs = actor.getAllLogEntries();
+        assertEquals(logs.size(), 1);
+        assertEquals(logs.get(0).message(), "null");
+        assertEquals(logs.get(0).level(), Level.DEBUG);
     }
 }
