4114ab39e13d0d1daf1048194d2a379ec3e11b50
shyam <shyamsundar.shanmugam@wipro.com>
2021-11-16 18:31:40 +0530

    implementing worker and test harness

--- src/main/java/com/example/blockchain/actors/WorkerBehavior.java
+++ src/main/java/com/example/blockchain/actors/WorkerBehavior.java
@@ -6,6 +6,9 @@ import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Receive;
 import akka.actor.typed.javadsl.Behaviors;
 import com.example.blockchain.model.Block;
+import com.example.blockchain.model.HashResult;
+
+import static com.example.blockchain.utils.BlockChainUtils.calculateHash;
 
 public class WorkerBehavior extends AbstractBehavior<WorkerBehavior.Command> {
 	
@@ -43,7 +46,35 @@ public class WorkerBehavior extends AbstractBehavior<WorkerBehavior.Command> {
 	@Override
 	public Receive<Command> createReceive() {
 		// TODO Auto-generated method stub
-		return null;
+		return newReceiveBuilder()
+				.onAnyMessage(cmd -> {
+					Block block = cmd.getBlock();
+					int difficultyLevel = cmd.getDifficulty(),
+							startNonce = cmd.getStartNonce(),
+							endNonce = startNonce + 1000;
+
+					String hash = new String(new char[difficultyLevel]).replace("\0", "X");
+					String target = new String(new char[difficultyLevel]).replace("\0", "0");
+
+					int nonce = startNonce;
+					while (!hash.substring(0,difficultyLevel).equals(target) && nonce < endNonce) {
+						nonce++;
+						String dataToEncode = block.getPreviousHash() + Long.toString(block.getTransaction().getTimestamp()) + Integer.toString(nonce) + block.getTransaction();
+						hash = calculateHash(dataToEncode);
+					}
+					if (hash.substring(0,difficultyLevel).equals(target)) {
+						HashResult hashResult = new HashResult();
+						hashResult.foundAHash(hash, nonce);
+						getContext().getLog().debug(hashResult.getNonce() +  " : " + hashResult.getHash());
+						return Behaviors.same();
+					}
+					else {
+						getContext().getLog().debug("null");
+						return Behaviors.same();
+					}
+				})
+				.build();
 	}
+
 
 }
---
+++ src/test/java/com/example/blockchain/MiningTests.java
@@ -0,0 +1,12 @@
+package com.example.blockchain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MiningTests {
+    @Test
+    void example() {
+        fail();
+    }
+}
